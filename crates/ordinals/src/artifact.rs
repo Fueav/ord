e7a04432cd61cf2fec62a9ec8dcf34f53f6e285a
Fueav <fueave@gmail.com>
2025-03-04 17:20:21 +0800

    fix: add des

--- crates/ordinals/src/artifact.rs
+++ crates/ordinals/src/artifact.rs
@@ -1,5 +1,5 @@
 use super::*;
-
+use serde::{Serialize, Serializer};
 #[derive(Serialize, Eq, PartialEq, Deserialize, Debug)]
 pub enum Artifact {
   Cenotaph(Cenotaph),
@@ -13,4 +13,24 @@ impl Artifact {
       Self::Runestone(runestone) => runestone.mint,
     }
   }
+}
+
+pub fn u128_op_to_string_serialize<S>(x: &Option<u128>, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+  if let Some(x) = x {
+    return s.serialize_str(&x.to_string());
+  } else {
+    return s.serialize_str("0");
+  }
+
+}
+
+pub fn u128_to_string_serialize<S>(x: &u128, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+  return s.serialize_str(&x.to_string());
+
 }
